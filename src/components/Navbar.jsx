dbdb8b2d4be5d823df3e62c19d3e232353505116
julfikar <infojulfikarchowdhury@gmail.com>
2025-01-30 23:08:07 +0600

    navbar added for both page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { NavLink, useLocation } from "react-router";
 
 const Navbar = () => {
   const [theme, setTheme] = useState("light");
@@ -8,10 +9,31 @@ const Navbar = () => {
     setTheme(newTheme);
     document.documentElement.setAttribute("data-theme", newTheme);
   };
+  const location = useLocation();
+  console.log(location);
 
   return (
     <div className="navbar">
       <h1 className="header">Habit Tracker</h1>
+      <nav>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? "active nav-link" : "nav-link"
+          }
+        >
+          Home
+        </NavLink>
+        &nbsp;|&nbsp;
+        <NavLink
+          to="/analytics"
+          className={({ isActive }) =>
+            isActive ? "active nav-link" : "nav-link"
+          }
+        >
+          Analytics
+        </NavLink>
+      </nav>
       <button className="toggle-btn" onClick={toggleTheme}>
         {theme === "light" ? "🌙" : "🌞"}
       </button>
